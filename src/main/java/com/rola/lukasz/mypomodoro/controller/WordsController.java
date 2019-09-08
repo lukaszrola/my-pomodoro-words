aa6397adb69d232341876770df8e4c1d13f15f2e
lukasz <lukasz.rola@wp.pl>
2019-09-08 09:56:35 +0200

    Add prototype of questions endpoint

--- src/main/java/com/rola/lukasz/mypomodoro/controller/WordsController.java
+++ src/main/java/com/rola/lukasz/mypomodoro/controller/WordsController.java
@@ -1,6 +1,8 @@
 package com.rola.lukasz.mypomodoro.controller;
 
+import com.rola.lukasz.mypomodoro.model.Question;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -9,7 +11,16 @@ import java.util.List;
 public class WordsController {
 
     @GetMapping("/categories")
-    public List<String> getCategories(){
+    public List<String> getCategories() {
         return List.of("english words", "german words");
+    }
+
+    @GetMapping("/questions")
+    public List<Question> getQuestions(@RequestParam Integer numberOfWords, @RequestParam String category) {
+        return List.of(
+                new Question("dog", "pies", List.of("mouse", "cat", "bird")),
+                new Question("apple", "jablko", List.of("cherry", "pineaple", "blueberry"))
+        );
+
     }
 }
