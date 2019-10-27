65500e0751a414c5f3e777c9dbb521c735c14e01
lukasz <lukasz.rola@wp.pl>
2019-10-27 07:08:50 +0100

    MP-24 Add integration test

--- src/test/java/com/rola/lukasz/mypomodoro/MyPomodoroApplicationTests.java
+++ src/test/java/com/rola/lukasz/mypomodoro/MyPomodoroApplicationTests.java
@@ -1,16 +1,35 @@
 package com.rola.lukasz.mypomodoro;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class MyPomodoroApplicationTests {
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-	@Test
-	public void contextLoads() {
-	}
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class MyPomodoroApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getOneWordTest() throws Exception {
+        mockMvc
+                .perform(get("/foreignLanguageChoiceQuestions?numberOfWords=1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("question")))
+        ;
+    }
 
 }
