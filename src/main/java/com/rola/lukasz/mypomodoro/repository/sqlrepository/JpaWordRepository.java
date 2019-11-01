d310f25d8951858845302e6820cd8b83b583e139
lukasz <lukasz.rola@wp.pl>
2019-11-01 19:14:30 +0100

    MP-31 Cleanup: Remove DummyWordRepository

--- src/main/java/com/rola/lukasz/mypomodoro/repository/sqlrepository/JpaWordRepository.java
+++ src/main/java/com/rola/lukasz/mypomodoro/repository/sqlrepository/JpaWordRepository.java
@@ -1,14 +1,12 @@
 package com.rola.lukasz.mypomodoro.repository.sqlrepository;
 
 import com.rola.lukasz.mypomodoro.repository.WordRepository;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-@ConditionalOnProperty(value = "mypomodoro.wordrepository", havingValue = "jpaWordRepository")
 interface JpaWordRepository extends CrudRepository<JpaWord,Long>, WordRepository {
     @Override
     default List<JpaWord> getWords() {
